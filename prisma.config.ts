b2790f0a36afde9f84a7aac368b9079cb1e1e937
Roberto Silva <robertocunha.silva@gmail.com>
2026-04-05 00:20:08 -0300

    banco de dados criado e migrado

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,4 +1,8 @@
 import { defineConfig } from '@prisma/config'
 import 'dotenv/config'
 
-export default defineConfig({})
+export default defineConfig({
+  datasource: {
+    url: process.env.DATABASE_URL, // Ele vai ler do seu arquivo .env
+  },
+});
